88a9e1b43a5929e37224cee224fd67795b7cd741
youngoo <1965214573@qq.com>
2021-12-11 20:55:01 +0800

    修复后端返回中文乱码问题

--- src/java/com/example/fliter/CharacterEncodeFilter.java
+++ src/java/com/example/fliter/CharacterEncodeFilter.java
@@ -2,6 +2,7 @@ package com.example.fliter;
 
 import javax.servlet.*;
 import javax.servlet.annotation.*;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 /**
@@ -22,6 +23,7 @@ public class CharacterEncodeFilter implements Filter {
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws ServletException, IOException {
         request.setCharacterEncoding("UTF-8");
         response.setCharacterEncoding("UTF-8");
+        ((HttpServletResponse) response).setHeader("Content-Type", "application/json; charset=utf-8");
         System.out.println("已设置编码");
         chain.doFilter(request, response);
     }

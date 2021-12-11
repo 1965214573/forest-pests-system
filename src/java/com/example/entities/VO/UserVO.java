4ee54b697b6f96a9d7c32f801aab31a5ea2097fb
youngoo <1965214573@qq.com>
2021-12-11 20:59:04 +0800

    调整代码

--- src/java/com/example/entities/VO/UserVO.java
+++ src/java/com/example/entities/VO/UserVO.java
@@ -1,12 +1,15 @@
 package com.example.entities.VO;
 
+import lombok.Builder;
+import lombok.Data;
 import lombok.ToString;
 
 /**
  * @author youngoo
  * @date 2021/12/9 19:05
  */
-@ToString
+@Data
+@Builder
 public class UserVO {
 
     private Integer userId;

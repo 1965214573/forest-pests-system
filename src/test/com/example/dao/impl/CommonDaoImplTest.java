06e8a1f0a4d564867f56908418ff3e148c826668
youngoo <1965214573@qq.com>
2021-12-16 18:24:13 +0800

    测试文件

--- src/test/com/example/dao/impl/CommonDaoImplTest.java
+++ src/test/com/example/dao/impl/CommonDaoImplTest.java
@@ -1,8 +1,24 @@
 package com.example.dao.impl;
 
+import com.example.entities.PO.User;
+import com.example.entities.VO.MenuVO;
+import com.example.mapper.CommonMapper;
+import com.example.utils.MybatisUtil;
+import org.apache.ibatis.session.SqlSession;
+import org.junit.Test;
+
+import java.util.List;
+
 public class CommonDaoImplTest {
 
-    public static void main(String[] args) {
 
+    @Test
+    public void testMenuList() {
+        try (SqlSession session = MybatisUtil.getSession()) {
+            CommonMapper mapper = session.getMapper(CommonMapper.class);
+
+            List<MenuVO> menuList = mapper.getMenuList(User.builder().userId(1).build());
+            menuList.forEach(System.out::println);
+        }
     }
 }

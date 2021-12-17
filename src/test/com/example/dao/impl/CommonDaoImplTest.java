f2d6bb06b062bead0d5b14cb3761a6027986d43b
youngoo <1965214573@qq.com>
2021-12-17 20:56:03 +0800

    优化项目结构

--- src/test/com/example/dao/impl/CommonDaoImplTest.java
+++ src/test/com/example/dao/impl/CommonDaoImplTest.java
@@ -1,8 +1,10 @@
 package com.example.dao.impl;
 
+import com.example.entities.PO.Menu;
 import com.example.entities.PO.User;
 import com.example.entities.VO.MenuVO;
 import com.example.mapper.CommonMapper;
+import com.example.mapper.TestDemoMapper;
 import com.example.utils.MybatisUtil;
 import org.apache.ibatis.session.SqlSession;
 import org.junit.Test;
@@ -19,6 +21,15 @@ public class CommonDaoImplTest {
 
             List<MenuVO> menuList = mapper.getMenuList(User.builder().userId(1).build());
             menuList.forEach(System.out::println);
+        }
+    }
+
+    @Test
+    public void testMenuIf() {
+        try (SqlSession session = MybatisUtil.getSession()) {
+            TestDemoMapper mapper = session.getMapper(TestDemoMapper.class);
+            List<Menu> menus = mapper.testIf(Menu.builder().menuName("首页").build());
+            menus.forEach(System.out::println);
         }
     }
 }

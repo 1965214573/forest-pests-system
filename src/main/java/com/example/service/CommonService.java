d1ebf28138e2df3d5dc76e878f89a4ec03af7270
youngoo <1965214573@qq.com>
2021-12-20 13:06:44 +0800

    添加maven支持

---
+++ src/main/java/com/example/service/CommonService.java
@@ -0,0 +1,26 @@
+package com.example.service;
+
+import com.example.entities.PO.User;
+import com.example.utils.ResultInfo;
+
+/**
+ * @author youngoo
+ */
+public interface CommonService {
+
+    /**
+     * 根据角色id获取menu列表
+     * 嵌套查询再进行封装整个内容
+     * @param roleId 角色id
+     * @return 直接返回一个具有树状结构的数组
+     */
+    ResultInfo showMenuList(Integer roleId);
+
+    /**
+     * 根据用户获取menu列表
+     * 嵌套查询再进行封装整个内容
+     * @param user 角色id
+     * @return 直接返回一个具有树状结构的数组
+     */
+    ResultInfo showMenuList(User user);
+}

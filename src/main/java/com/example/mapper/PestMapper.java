cb1ed6c99e6eee4d06a3a191e58636d017f9078e
youngoo <1965214573@qq.com>
2021-12-22 22:05:14 +0800

    新增虫害条件查询功能

--- src/main/java/com/example/mapper/PestMapper.java
+++ src/main/java/com/example/mapper/PestMapper.java
@@ -1,6 +1,7 @@
 package com.example.mapper;
 
 import com.example.entities.PO.Pest;
+import com.example.entities.Query.QueryPest;
 import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
@@ -22,4 +23,18 @@ public interface PestMapper {
      * @return 结果
      */
     List<Pest> selectPest(@Param("page") int page, @Param("size") int size);
+
+    /**
+     * 条件统计数量
+     * @param queryPest 查询条件
+     * @return 数量
+     */
+    int countAll(QueryPest queryPest);
+
+    /**
+     * 条件查询所有的虫害信息
+     * @param queryPest 查询条件
+     * @return 查询到的结合
+     */
+    List<Pest> queryAll(QueryPest queryPest);
 }

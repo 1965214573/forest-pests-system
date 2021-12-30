44a06d4f7091faa38bc20ef86df4d6fb607dfcb4
youngoo <1965214573@qq.com>
2021-12-30 16:46:00 +0800

    fix(view): 动态适应部署环境
    修复前端页面统一请求后端的接口地址，目前改为自动获取当前部署信息，前后端分离时，需要手动指定后端地址

--- src/main/webapp/api/config/global.js
+++ src/main/webapp/api/config/global.js
@@ -1,6 +1,17 @@
 layui.define(function (exports) {
     exports('setting', {
-        baseURL: 'http://localhost:8080',
+        baseURL: getRootPath(), // 后端接口路径
         dataType: 'json'
     })
+
+    /**
+     * 获取当前项目部署环境，前后端分离时，可指定具体后端接口路径
+     * @returns {string}
+     */
+    function getRootPath() {
+        let curWwwPath=window.document.location.href;
+        let pathName=window.document.location.pathname;
+        let pos=curWwwPath.indexOf(pathName);
+        return curWwwPath.substring(0,pos)
+    }
 })
